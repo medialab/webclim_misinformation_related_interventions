11e2e74f7225c3be72a67c85a4ecab958d5c2f70
Danaal <danaalmanla@hotmail.com>
2021-06-15 09:21:39 +0200

    Update:collect_youtube_data.py

--- code/collect_youtube_data.py
+++ code/collect_youtube_data.py
@@ -1,6 +1,8 @@
 from googleapiclient.discovery import build
 from datetime import datetime, timedelta
 import pandas as pd
+import numpy as np
+import re
 import sys
 
 
@@ -32,6 +34,9 @@ def timestamp(video):
 def video_title(video):
     return video['snippet']['title']
 
+def video_id(video):
+        return video['snippet']['resourceId']['videoId']
+
 def collect_data(API_key, channel_id):
     youtube = build('youtube', 'v3', developerKey=API_key)
 
@@ -41,10 +46,62 @@ def collect_data(API_key, channel_id):
     titles = [video_title(video) for video in videos]
 
     publish_timestamps = [timestamp(video) for video in videos]
+    video_ids = [video_id(video) for video in videos]
+    videos_df = pd.DataFrame({'Upload_date':publish_timestamps,'video_titles':titles,'video_id':video_ids})
+    videos_df = videos_df[videos_df['Upload_date']>'2019-01-01']
+    videos_ids_filtered = list(videos_df['video_id'])
+    data = pd.DataFrame([], columns=[ 'video_title', 'view_counts', 'likes', 'dislikes', 'comments', 'video_id',
+                                       'channel_name', 'channel_id', 'published_at', 'duration'])
+    for i in videos_ids_filtered:
+        video_data = get_data_video(i, youtube)
+        r_series = pd.Series(video_data, index=data.columns)
+        data = data.append(r_series, ignore_index=True)
+    channel_name = data['channel_name'].iloc[0]
+    path = 'data/' + channel_name + '_youtube_data.csv'
+    data.to_csv(path)
 
-    videos_df = pd.DataFrame({'Upload_date':publish_timestamps,'video_titles':titles})
-    videos_df.to_csv('data/youtube_data.csv')
 
+def get_data_video( vid_id,youtube):
+        data = np.array([])
+        vid_suff = vid_id
+        request = youtube.videos().list(
+            part="snippet,contentDetails,statistics",
+            id=vid_suff
+        )
+        response = request.execute()
+        try:
+            # print(response)
+
+            main_info = response['items'][0]['snippet']
+
+            channel_name = main_info['channelTitle']
+            video_title = main_info['title']
+            channel_ids = main_info['channelId']
+            published_ats = main_info['publishedAt']
+            video_ids = response['items'][0]['id']
+            view_counts = response['items'][0]['statistics']['viewCount']
+            try:
+                likes = response['items'][0]['statistics']['likeCount']
+            except:
+                likes = 'non'
+            try:
+                dislikes = response['items'][0]['statistics']['dislikeCount']
+            except:
+                dislikes = 'non'
+            try:
+                comments = response['items'][0]['statistics']['commentCount']
+            except:
+                comments = 'non'
+            duration = re.findall(r'\d+', response['items'][0]['contentDetails']['duration'])
+
+            data = np.append(data, np.array(
+                [video_title, view_counts, likes, dislikes, comments, video_ids, channel_name, channel_ids,
+                 published_ats, duration]))
+
+            # data = np.append(data)
+        except:
+            print('An issue with collecting a video it can be that you run out of tokens in youtube API')
+        return data
 
 if __name__=="__main__":
 
