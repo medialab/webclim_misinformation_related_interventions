cc514ba9f6c10575a3ed6948aa135821c2db3e91
heloisethero <thero.heloise@gmail.com>
2021-07-15 14:14:55 +0200

    add temp code for scraping the flag on Facebook posts

--- code/collect_facebook_crowdtangle_false_link.sh
+++ code/collect_facebook_crowdtangle_false_link.sh
@@ -5,4 +5,10 @@ TODAY_DATE=$(date +"%Y-%m-%d")
 minet ct search \
 "https://beforeitsnews.com/eu/2021/04/stay-away-from-the-vaxxed-it-is-official-from-pfizers-own-documents-2671454.html" \
   --search-field include_query_strings --platform facebook --start-date 2019-01-01 > \
-  "./data/facebook_crowdtangle_false_link_${TODAY_DATE}.csv"
+  "./data/facebook_crowdtangle_raw_false_link_${TODAY_DATE}.csv"
+
+# On minet's new banch only for now. 
+# TO DO: change when minet's new version will be released.
+
+lminet fb post-stats post_url "./data/facebook_crowdtangle_raw_false_link_${TODAY_DATE}.csv" \
+  -s post_url > "./data/facebook_crowdtangle_false_link_${TODAY_DATE}.csv"
